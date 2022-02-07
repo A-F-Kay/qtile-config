57f8f8e9e81e24f2e6fca3864ac726f3ca039b20
A. F. Kay <afkay.dev@pm.me>
2022-02-07 16:12:24 +0200

    Upd layouts + Fix border color

--- afkay_config/layouts/const.py
+++ afkay_config/layouts/const.py
@@ -2,16 +2,16 @@ from libqtile import layout
 
 
 layout_cfg = {
-    'margin': 3,
+    'margin': 8,
     'margin_on_single': 15,
-    'border_focus': '#F-1D9FF',
+    'border_focus': '#F1D9FF',
     'border_normal': '#49453c',
-    'border_width': 1,
+    'border_width': 2,
     'border_on_single': True,
 }
 
 layouts = [
     layout.Columns(**layout_cfg),
-    layout.MonadTall(**layout_cfg),
+    layout.Tile(**layout_cfg),
     layout.Max(),
 ]
